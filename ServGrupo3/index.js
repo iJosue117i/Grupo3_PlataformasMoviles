8dbaf47f1ed5a97d9606881e5d84b6b25ff71b21
JosueACR <josuecastillo117@gmail.com>
2024-07-01 21:29:31 -0500

    index

--- ServGrupo3/index.js
+++ ServGrupo3/index.js
@@ -6,6 +6,41 @@ const app = express()
 
 const PUERTO=3000
 
+const conexion = mysql.createConnection({
+    host:'localhost',
+    database:'metropolitano',
+    user:'root',
+    password:'123456'
+})
+
 app.listen(PUERTO,()=>{
     console.log('Servidor corriendo en '+ PUERTO)
+})
+
+app.get('/',(req,res)=>{
+    res.send('Bienvenido al servicio de metropolitano')
+})
+
+app.get('/clientes',(req,res)=>{
+    const consulta=" SELECT * FROM clientes"
+    conexion.query(consulta,(error,resultado)=>{
+        if(error) return console.error(error.message)
+            
+        const obj={}
+
+        if(resultado.length>0){
+            obj.listaPersonas=resultado
+            res.json(obj)
+        }else{
+            res.json('no hay clientes')
+        }
+    })
+})
+
+app.post('/clientes/registro',(req,res)=>{
+    const cliente={
+        cli_nombre:req.body.cli_nombre,
+        cli_apellido:req.body.cli_apellido,
+        cli_correo:req.body.cli_correo
+    }
 })
