b60a4b32dc5ea23776417c0685b225e9ca40f776
JosueACR <josuecastillo117@gmail.com>
2024-07-02 02:20:43 -0500

    Servicios

--- ServGrupo3/index.js
+++ ServGrupo3/index.js
@@ -10,7 +10,7 @@ const conexion = mysql.createConnection({
     host:'localhost',
     database:'proyectomoviles',
     user:'root',
-    password:'mysql'
+    password:'123456'
 })
 
 app.listen(PUERTO,()=>{
@@ -22,17 +22,17 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/cliente',(req,res)=>{
-    const consulta=" SELECT * FROM cliente"
+    const consulta=" SELECT * FROM cliente "
     conexion.query(consulta,(error,resultado)=>{
         if(error) return console.error(error.message)
             
         const obj={}
 
         if(resultado.length>0){
-            obj.listaPersonas=resultado
+            obj.listaClientes=resultado
             res.json(obj)
         }else{
-            res.json('no hay cliente')
+            res.json('no hay clientes')
         }
     })
 })
@@ -41,6 +41,60 @@ app.post('/cliente/registro',(req,res)=>{
     const cliente={
         cli_nombre:req.body.cli_nombre,
         cli_apellido:req.body.cli_apellido,
-        cli_correo:req.body.cli_correo
+        cli_correo:req.body.cli_correo,
+        cli_contraseña:req.body.cli_contraseña,
+        fk_tarj_id:req.body.fk_tarj_id
     }
+
+    const query = "INSERT INTO cliente SET?"
+    conexion.query(query,cliente,(error)=>{
+        if(error) return console.error(error.message)
+        res.json("Se inserto correctamente a la persona")
+    })
+})
+
+app.get('/login',(req,res)=>{
+    const {cli_correo, cli_contraseña}=req.params
+    const query="SELECT * FROM cliente WHERE cli_correo=? AND cli_contraseña=?"
+
+    conexion.query(query,[cli_correo,cli_contraseña],(error,respuesta)=>{
+        if(error) return console.error(error.message)
+
+        if(respuesta.length>0){
+            res.json(respuesta[0])
+        }else{
+            res.json("No hay registros")
+        }
+    })
+})
+
+app.get('/tarjeta',(req,res)=>{
+    const {cli_id}=req.params
+    const consulta="SELECT c.cli_id, tm.tarj_id, tm.tarj_saldo, tm.tipo_tarj_id FROM cliente c JOIN tarjeta_metro tm ON c.fk_tarj_id = tm.tarj_id WHERE c.cli_id = ?"  
+
+    conexion.query(express.query,[cli_id],(error, resultado)=>{
+        if(error) return console.error(error.message)
+
+        const obj={}
+
+        if(resultado.length>0){
+            obj.listaTarjetas=resultado
+            res.json(obj)
+        }else{
+            res.json('no hay tarjetas')
+        }
+    })
+})
+
+app.put('/recargar/:monto',(req,res)=>{
+    const {monto}=req.params
+    const {tarj_id} =req.body
+
+
+    const query="UPDATE tarjeta_metro SET `tarj_saldo` = `tarj_saldo` + "+monto+ "WHERE `tarj_id` = "+tarj_id
+    conexion.query(query,(error)=>{
+        if(error) return console.error(error.message)
+
+        res.json("Se recargo la tarjeta con exito")
+    })
 })
