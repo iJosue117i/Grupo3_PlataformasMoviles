97368c2dba2357b03a64ad8ac8fa300751aed2d0
pSergio91 <u202322010@upc.edu.pe>
2024-07-03 00:33:43 -0500

    pantalla

---
+++ AndroidGrupo3/app/src/main/java/com/upc/grupo3/RegistroCliente.kt
@@ -0,0 +1,20 @@
+package com.upc.grupo3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistroCliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro_cliente)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
