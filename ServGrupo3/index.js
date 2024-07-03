4c672a4b05d8b5ba460379827a004adfd7159a08
JosueACR <josuecastillo117@gmail.com>
2024-07-02 22:28:20 -0500

    Sevicios

--- ServGrupo3/index.js
+++ ServGrupo3/index.js
@@ -31,7 +31,7 @@ app.get('/cliente',(req,res)=>{
         if(resultado.length>0){
             obj.listaClientes=resultado
             res.json(obj)
-        }else{
+         }else{
             res.json('no hay clientes')
         }
     })
@@ -70,9 +70,9 @@ app.get('/login',(req,res)=>{
 
 app.get('/tarjeta',(req,res)=>{
     const {cli_id}=req.params
-    const consulta="SELECT c.cli_id, tm.tarj_id, tm.tarj_saldo, tm.tipo_tarj_id FROM cliente c JOIN tarjeta_metro tm ON c.fk_tarj_id = tm.tarj_id WHERE c.cli_id = ?"  
+    const query="SELECT c.cli_id, tm.tarj_id, tm.tarj_saldo, tm.tipo_tarj_id FROM cliente c JOIN tarjeta_metro tm ON c.fk_tarj_id = tm.tarj_id WHERE c.cli_id = ?"  
 
-    conexion.query(express.query,[cli_id],(error, resultado)=>{
+    conexion.query(query,[cli_id],(error, resultado)=>{
         if(error) return console.error(error.message)
 
         const obj={}
@@ -83,6 +83,16 @@ app.get('/tarjeta',(req,res)=>{
         }else{
             res.json('no hay tarjetas')
         }
+    })
+})
+
+app.delete('/tarjeta/eliminar/:tarj_id',(req,res)=>{
+    const {tarj_id}=req.params
+
+    const query ="DELETE FROM recargas WHERE tarjeta_metro_id = "+tarj_id
+    conexion.query(query,(error)=>{
+        if(error) console.error(error.message)
+        res.json("Se elimino tarjeta")
     })
 })
 
@@ -98,3 +108,56 @@ app.put('/recargar/:monto',(req,res)=>{
         res.json("Se recargo la tarjeta con exito")
     })
 })
+
+app.post('/recarga',(req,res)=>{
+    const recarga={
+        tarjeta_metro_id:req.body.tarjeta_metro_id,
+        tarjeta_metro_tipo_id:req.body.tarjeta_metro_tipo_id,
+        pago_debito_id:req.body.pago_debito_id,
+        recargas_fecha:new Date(),
+        recarga_monto:req.body.recarga_monto,
+        observacion:req.body.observacion
+    }
+
+    const query = "INSERT INTO recargas SET?"
+    conexion.query(query,recarga,(error)=>{
+        if(error) return console.error(error.message)
+        res.json("Se inserto correctamente la recarga")
+    })
+})
+
+app.get('/recarga/historial',(req,res)=>{
+    const {cli_id}=req.params
+    const query="SELECT * FROM recargas r JOIN tarjeta_metro tm ON r.tarjeta_metro_id = tm.tarj_id JOIN cliente c ON c.fk_tarj_id=tm.tarj_id WHERE c.cli_id =?"
+
+    conexion.query(query,[cli_id],(error, resultado)=>{
+        if(error) return console.error(error.message)
+
+        const obj={}
+
+        if(resultado.length>0){
+            obj.listarRecargas=resultado
+            res.json(obj)
+        }else{
+            req.json('no hay recargas')
+        }
+    })
+})
+
+
+app.post('/tarjeta/debito',(req,res)=>{
+    const debito={
+        debito_id:req.body.debito_id,
+        debit_num_tarjeta:req.body.debit_num_tarjeta,
+        debito_fecha_caducidad:req.body.debito_fecha_caducidad,
+        debito_ccv:req.body.debito_ccv
+    }
+
+    const query = "INSERT INTO pago_debito SET?"
+    conexion.query(query,debito,(error)=>{
+        if(error) return console.error(error.message)
+        res.json("Se inserto la tarjeta de debito")
+    })
+})
+
+
