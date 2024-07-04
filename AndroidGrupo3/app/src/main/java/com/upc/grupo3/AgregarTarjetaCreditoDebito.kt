d1b6e3a5d47dfa609306e180baa81adce0e89d49
KarenAriZ <89676364+KarenAriZ@users.noreply.github.com>
2024-07-03 21:27:57 -0500

    ulti

--- AndroidGrupo3/app/src/main/java/com/upc/grupo3/AgregarTarjetaCreditoDebito.kt
+++ AndroidGrupo3/app/src/main/java/com/upc/grupo3/AgregarTarjetaCreditoDebito.kt
@@ -1,12 +1,15 @@
 package com.upc.grupo3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AgregarTarjetaCreditoDebito : AppCompatActivity() {
+    private lateinit var btnGuardar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +18,16 @@ class AgregarTarjetaCreditoDebito : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        llenarDatos()
+    }
+    private fun llenarDatos() {
+        btnGuardar = findViewById(R.id.btnGuardar)
+
+        btnGuardar = findViewById(R.id.btnGuardar)
+        btnGuardar.setOnClickListener {
+            val intent = Intent(this, Pago::class.java)
+            startActivity(intent)
         }
     }
 }
--- AndroidGrupo3/app/src/main/java/com/upc/grupo3/RecargarTarjeta.kt
+++ AndroidGrupo3/app/src/main/java/com/upc/grupo3/RecargarTarjeta.kt
@@ -1,12 +1,17 @@
 package com.upc.grupo3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RecargarTarjeta : AppCompatActivity() {
+    private lateinit var btnPagar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,15 @@ class RecargarTarjeta : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        asignarRefe()
     }
+        private fun asignarRefe() {
+            btnPagar = findViewById(R.id.btnPagar)
+
+            btnPagar = findViewById(R.id.btnPagar)
+            btnPagar.setOnClickListener {
+                val intent = Intent(this, AgregarTarjetaCreditoDebito::class.java)
+                startActivity(intent)
+            }
+        }
 }
